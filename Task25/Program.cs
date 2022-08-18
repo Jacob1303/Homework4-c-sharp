4704afaf56dd2768323443e1efedfb8d75a117c4
yakov kuzin <kuzin_js@mail.ru>
2022-08-18 21:26:00 +0300

    Task 25 added

---
+++ Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programm
+{
+    static int Exponent(int a, int b)
+    {
+        int expo = 1;
+
+        for (int i = 0; i < b; i++)
+        {
+            expo = expo * a;
+        }
+        return expo;
+        
+        Console.WriteLine("Первое число возведённое в степень второго числа = " + expo);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите 2 положительных целых числа");
+        Console.Write("Первое число - ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Второе число - ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Первое число возведённое в степень второго числа = " + Exponent(num1, num2));
+    }
+}
