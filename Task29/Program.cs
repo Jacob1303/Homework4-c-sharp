fc448299e821703cd05704dd16bbeb9e9eacdadf
yakov kuzin <kuzin_js@mail.ru>
2022-08-18 21:39:17 +0300

    Task 29  added

---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Programm
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("ВВедите предполагаемое количество используемых чисел - ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double[] array = new double[n];
+
+        Console.WriteLine("Введите поочередно числа");
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
