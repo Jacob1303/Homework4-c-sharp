adc3901fa8fbb6417b527f69e055ce51e9ba7265
yakov kuzin <kuzin_js@mail.ru>
2022-08-18 21:31:59 +0300

    Task 27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programm
+{
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        string numb = Console.ReadLine();
+        int numbConv = Convert.ToInt32(numb);
+        int[] array = new int[numb.Length];
+        int j = 0;
+
+        for (int i = 0; i < numb.Length; i++)
+        {
+            int figure = numbConv % 10;
+            numbConv = numbConv / 10;
+            array[j] = figure;
+            j++;
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < j; i++)
+        {
+            sum = sum + array[i];
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
